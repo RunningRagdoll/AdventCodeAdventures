e560bcdaa6a87e7d5aa21a5c835fcc99b0754971
Adam Edlund <adam92edlund@gmail.com>
2019-12-04 21:59:25 +0100

    Day1 setup

---
+++ Day1/FuelCounter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day1
+{
+    public class FuelCounter
+    {
+        private string ListOfModules;
+        public string[] loadModuleMass(string ListOfModules)
+        {
+            throw new NotImplementedException("Please create a test first");
+        } 
+    }
+}
